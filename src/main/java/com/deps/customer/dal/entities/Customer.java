c824d1c974022b5cfd2be1c2e1fca25a99f1972f
dendeps <dendeps@gmail.com>
2018-06-07 23:40:45 +0800

    Finished task, tests work well

---
+++ src/main/java/com/deps/customer/dal/entities/Customer.java
@@ -0,0 +1,43 @@
+package com.deps.customer.dal.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customertab")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY )
+	private Long id;
+	@Column(name="sname")
+	private String name;
+	@Column(name="semail")
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+}
---
+++ src/test/java/com/deps/customer/dal/CustomerdataApplicationTests.java
@@ -0,0 +1,47 @@
+package com.deps.customer.dal;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.deps.customer.dal.entities.Customer;
+import com.deps.customer.dal.repos.CustomerRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerdataApplicationTests {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Test
+	public void testCreateCustomer() {
+		Customer customer = new Customer();
+		customer.setName("John");
+		customer.setEmail("john@gmail.com");
+		Customer result = customerRepository.save(customer);
+		System.out.println(result);
+	}
+	
+	@Test
+	public void testFindCustomerById() {
+		Customer customer = customerRepository.findById(1l).get();
+		System.out.println(customer);
+	}
+	
+	@Test
+	public void testUpdateCustomert() {
+		Customer customer = customerRepository.findById(1l).get();
+		System.out.println(customer);
+		customer.setEmail("johny.walker@gmail.com");
+		System.out.println(customer);
+		customerRepository.save(customer);
+	}
+	
+	public void testDeleteCustomer() {
+		customerRepository.deleteById(1l);
+	}
+
+}
